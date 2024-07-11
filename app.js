cbbe59da73e5f18c27936b6ac021732a4909df1f
Simmonds <jake.simmonds@capgemini.com>
2024-07-11 11:31:27 +0100

    updated ideas list and stylint

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var ideas = [
-    '1', '2', '3', '4', '5'
+    'Portfolio Website', 'Mini Games', 'Front-end Landing Page', 'Tournement Simulation Application', 'Chess AI Bot', 'Employee Training Gameified App', 'To-do List App',
+    'Personal Finance Tracker', 'Calculator', 'Neural Network', 'Build API', 'Real-time Chat Application', 'Algorithm Visualiser', 'Travel Booking System', 'Quiz Application',
+    'HTTP Server', 'Real-time Editor', 'Chat-bot', 'QR Code Generator', 'Sentiment Analysis Tool', 'Online Multiplayer Game'
 ]
 
 const idea = document.getElementById('projectText');
 const ideaBtn = document.getElementById('newProjectBtn');
 const copyBtn = document.getElementById('copyIdeaBtn');
+let textArea = document.createElement('textarea');
 
 ideaBtn.addEventListener('click', projectFn);
 copyBtn.addEventListener('click', copyFn);
@@ -12,13 +15,19 @@ projectFn();
 
 function projectFn() {
     var randomNumber = Math.floor(Math.random() * (ideas.length));
-    document.getElementById('projectText').innerHTML = ideas[randomNumber];
+    idea.innerHTML = ideas[randomNumber];
 }
 
 function copyFn() {
-    var copyText = document.getElementById('projectText');
-    copyText.select();
-    // copyText.setSelectRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
-    alert("Copied the idea: " + copyText.value);
+    // idea.select();
+    // idea.setSelectRange(0, 99999);
+    // navigator.clipboard.writeText(idea.value);
+    // alert("Copied the idea: " + idea.value);
+
+    // var textArea = document.createElement('textarea');
+    textArea = idea.textContent;
+    textArea.select();
+    navigator.clipboard.writeText(textArea.value);
+    document.body.removeChild(textArea);
+    alert('Idea copied to clipboard!');
 }
